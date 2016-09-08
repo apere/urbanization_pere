135a4ddc743b2d51005c65c19cd95f5d06033311
adam m pere <adampere001@gmail.com>
2016-09-07 21:56:08 -0400

    nav (not functional)

--- index.php
+++ index.php
@@ -2,12 +2,25 @@
 
 get_header(); ?>
 
+	<nav>
+		<img id="office-logo" src="<?php echo get_bloginfo('template_directory');?>/assets/OFFICE_Logo_Final.svg">
+		<ul>
+			<a><li>Premises</li></a>
+			<a><li>Positions</li></a>
+			<a><li>Projects</li></a>
+			<a><li>People</li></a>
+			<a><li>Publics</li></a>
+		</ul>
+	</nav>
+
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
 		<?php
 	 		 // main content info here
 		?>
+			
+			
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
