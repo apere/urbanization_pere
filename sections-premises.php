c3ab60b33a51076deb5b5d8a33e5b203446d7bdc
adam m pere <adampere001@gmail.com>
2016-09-12 17:00:26 -0400

    meeting updates

--- sections-premises.php
+++ sections-premises.php
@@ -6,10 +6,7 @@
 				The Harvard Graduate School of Design’s Office for Urbanization draws upon the School’s history of design innovation to address societal and cultural conditions associated with contemporary urbanization. It develops speculative and projective urban scenarios through sponsored design research projects.
 			</p>
 			<p>
-				The Office imagines alternative and better futures through applied design research.
-			</p> 
-			<p>
-				It aspires to reduce the distance between design innovation and societal impact.
+				The Office imagines alternative and better futures through applied design research. It aspires to reduce the distance between design innovation and societal impact.
 			</p>			
 		</main>
 	</article>
