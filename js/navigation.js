e4183d82ac1c48d0c0e6a9b7709371e7b3c97f82
adam m pere <adampere001@gmail.com>
2016-09-11 23:42:42 -0400

    navigation

--- js/navigation.js
+++ js/navigation.js
@@ -7,26 +7,50 @@
 document.addEventListener('DOMContentLoaded', function() {
 	var firstClick = true;
 	var scroller = $('#primary');
-	
+	var inPost = $('.post');
 	var initBack = $('#initial-background');
 	var secBack = $('#primary');
+	var navBar = $('nav');
 	
-	//secBack.css("opacity", "0");
-	initBack.css("background-image", "url(" + templateUrl + "/assets/imgs/background-test.jpg)")
+	var url = document.URL;
+	var anchorIndex = url.indexOf("#");
+	
+	navBar.animate({
+					opacity: "1"
+				},10, function() {	});
+	
+	
+	if(inPost.get().length <= 0 && anchorIndex < 0){	// make sure premises is lined up 
+		secBack.scrollTop($('#premises').offset().top - 50 - $("#main").offset().top);
+	} 
+	
+	if(inPost.get().length <= 0 && anchorIndex < 0){ // in a post without an anchor?
+		initBack.animate({
+					opacity: "1"
+				},2000, function() {	});
+		initBack.css("background-image", "url(" + templateUrl + "/assets/imgs/background-test.jpg)");
+	} else {
+		initBack.css("display", "none");
+		secBack.animate({
+				opacity: "1"
+			},1000, function() {	});	
+	}
+	
+	
+	
+	
 	
   $('nav li').click(function() {
-		
-		
-		var anchor = $(this).data("link");
-		
+		var anchor = $(this).data("link");		
 		var aTag = $("#"+ anchor);
-		var location = aTag.offset().top - 50;
-		var off = $("#main").offset().top;
 		
-		location = location - off;
-		
-		if(firstClick) {
-			if(location != 0){
+		if(inPost.get().length <= 0){ // we are on the main page
+			var location = aTag.offset().top - 50;
+			var off = $("#main").offset().top;
+			location = location - off;
+
+			if(firstClick) {
+			if(location !== 0){
 				scroller.scrollTop(location);
 			}
 			initBack.animate({
@@ -34,13 +58,22 @@ document.addEventListener('DOMContentLoaded', function() {
 			},700, function() {
 				initBack.css('display', 'none');
 				firstClick = false;
-			})
+			});
+			secBack.animate({
+				opacity: "1"
+			},700, function() {	});
+
 		} else {
 			if(location !== 0){
 				scroller.animate({scrollTop: location},'1000');
 			}		
 		}
-		
-		
+		} else { // we're on a project page
+			var currURL = document.URL;
+			 
+			// get the 3rd '/' to parse out the projection part of the URL
+			currURL = currURL.substr(0,currURL.split("/", 3).join("/").length + 1) + "#" + anchor;
+			window.location.href = currURL;
+		}
 	}); 
 }, false);
