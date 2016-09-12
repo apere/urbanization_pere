6ff873e0f5a672858a9b6b2e34ca0f82a1867e4b
adam m pere <adampere001@gmail.com>
2016-09-11 21:07:17 -0400

    initial background image

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-
+	<div id="initial-background"></div>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
--- js/navigation.js
+++ js/navigation.js
@@ -5,8 +5,18 @@
  * navigation support for dropdown menus.
  */
 document.addEventListener('DOMContentLoaded', function() {
+	var firstClick = true;
 	var scroller = $('#primary');
+	
+	var initBack = $('#initial-background');
+	var secBack = $('#primary');
+	
+	//secBack.css("opacity", "0");
+	initBack.css("background-image", "url(" + templateUrl + "/assets/imgs/background-test.jpg)")
+	
   $('nav li').click(function() {
+		
+		
 		var anchor = $(this).data("link");
 		
 		var aTag = $("#"+ anchor);
@@ -14,9 +24,23 @@ document.addEventListener('DOMContentLoaded', function() {
 		var off = $("#main").offset().top;
 		
 		location = location - off;
-
-		if(location !== 0){
-			scroller.animate({scrollTop: location},'slow');
-		}		
+		
+		if(firstClick) {
+			if(location != 0){
+				scroller.scrollTop(location);
+			}
+			initBack.animate({
+				opacity: "0"
+			},700, function() {
+				initBack.css('display', 'none');
+				firstClick = false;
+			})
+		} else {
+			if(location !== 0){
+				scroller.animate({scrollTop: location},'1000');
+			}		
+		}
+		
+		
 	}); 
 }, false);
