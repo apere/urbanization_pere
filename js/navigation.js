277bd075c9e15718a6ce3b627ca3b450c17f1f0e
adam m pere <adampere001@gmail.com>
2016-09-12 01:57:19 -0400

    mobile menu up and running

--- js/navigation.js
+++ js/navigation.js
@@ -45,39 +45,86 @@ document.addEventListener('DOMContentLoaded', function() {
 	})
 	
   $('nav li div.link').click(function() {
-		var anchor = $(this).data("link");		
-		var aTag = $("#"+ anchor);
-		console.log(anchor);
-		
-		if(inPost.get().length <= 0){ // we are on the main page
-			var location = aTag.offset().top - 50;
-			var off = $("#main").offset().top;
-			location = location - off;
+		console.log(window.innerWidth);
+		if(window.innerWidth > 800) {
+			var anchor = $(this).data("link");		
+			var aTag = $("#"+ anchor);
+			console.log(anchor);
 
-			if(firstClick) {
+			if(inPost.get().length <= 0){ // we are on the main page
+				var location = aTag.offset().top - 50;
+				var off = $("#main").offset().top;
+				location = location - off;
+
+				if(firstClick) {
+					if(location !== 0){
+						scroller.scrollTop(location);
+					}
+					initBack.animate({
+						opacity: "0"
+					},700, function() {
+						initBack.css('display', 'none');
+						firstClick = false;
+					});
+					scroller.animate({
+						opacity: "1"
+					},700, function() {	});
+			} else {
 				if(location !== 0){
-					scroller.scrollTop(location);
+					scroller.animate({scrollTop: location},'1000');
+				}		
+			}
+			} else { // we're on a project page
+				var currURL = document.URL;
+
+				// get the 3rd '/' to parse out the projection part of the URL
+				currURL = currURL.substr(0,currURL.split("/", 3).join("/").length + 1) + "#" + anchor;
+				window.location.href = currURL;
+			}
+		} else { // **** MOBILE
+			
+			var anchor = $(this).data("link");		
+			var aTag = $("#"+ anchor + "-nav");
+			console.log(anchor);
+
+			if(inPost.get().length <= 0){ // we are on the main page
+				if(aTag.hasClass('active')) {
+					aTag.animate({
+							height: "toggle"
+						}, 300, function(){
+						aTag.removeClass("active");
+					});
+				} else {
+					$('.active').animate({
+							height: "toggle"
+						}, 200, function() {
+							$('.active').hide();
+							$('.active').removeClass("active");
+							aTag.addClass("active");
+							aTag.animate({
+									height: "toggle"
+								}, 500, function() {	});
+						});
+
+					setTimeout(function() {
+						if(!aTag.hasClass('active')) {
+							aTag.addClass("active");
+							aTag.animate({
+									height: "toggle"
+								}, 500, function() {	});
+						}
+					}, 300);
 				}
-				initBack.animate({
-					opacity: "0"
-				},700, function() {
-					initBack.css('display', 'none');
-					firstClick = false;
-				});
-				scroller.animate({
-					opacity: "1"
-				},700, function() {	});
-		} else {
-			if(location !== 0){
-				scroller.animate({scrollTop: location},'1000');
-			}		
+				
+			} else { // we're on a project page
+				var currURL = document.URL;
+
+				// get the 3rd '/' to parse out the projection part of the URL
+				currURL = currURL.substr(0,currURL.split("/", 3).join("/").length + 1) + "#" + anchor;
+				window.location.href = currURL;
+			}
+			
 		}
-		} else { // we're on a project page
-			var currURL = document.URL;
-			 
-			// get the 3rd '/' to parse out the projection part of the URL
-			currURL = currURL.substr(0,currURL.split("/", 3).join("/").length + 1) + "#" + anchor;
-			window.location.href = currURL;
-		}
+		
 	}); 
 }, false);
